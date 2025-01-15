01dec0d6a3eafa345d17850476ceac1b395691eb
TaljaaEb <138305368+TaljaaEb@users.noreply.github.com>
2025-01-15 12:02:36 +0200

    Update get_reply.py

--- bank/get_reply.py
+++ bank/get_reply.py
@@ -1,1 +1,14 @@
+import pydivert
+
+
+# Capture only TCP packets to port 80, i.e. HTTP requests.
+# packets will be captured from now on
+# Use WinDivert packet buffer
+with pydivert.WinDivert("tcp.DstPort == 80 and tcp.PayloadLength > 0") as buffer:
+    # Iterate the buffer
+    for packet in buffer:
+        payload = packet.payload
+        if bytes("SUCCESS", "utf-8") in payload:
+            print(packet.payload)
+        buffer.send(packet)
 
